f459e325b7b9b5c026ea7a42cf451046757d28d8
Bruno Costanzo <dev.bcostanzo@gmail.com>
2024-06-20 21:06:09 +0200

    first commit

---
+++ lib/boke_reports.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "boke_reports/version"
+
+module BokeReports
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/boke_reports_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BokeReports do
+  it "has a version number" do
+    expect(BokeReports::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
